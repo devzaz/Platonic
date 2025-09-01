2d80b86575cf8d42335b0a54264a0fdf458aec2d
devzaz <zubaerahmedjisan@gmail.com>
2025-09-01 22:12:58 +0600

    bug fixed

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,10 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', all_departments, name='all_dep'),
+    path('home', index, name='home'),
     path('accounts/login/', login_page, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('all-departments/', all_departments, name='all_dep'),
     path('executive-dashboard/', executive_dashboard, name='executive'),
     path('administration/', administration, name='administration'),  
 ]
