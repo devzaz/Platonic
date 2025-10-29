057015d05a7a42fd857c100fc54eab341b5419de
devzaz <zubaerahmedjisan@gmail.com>
2025-10-29 12:06:33 +0600

    minor changes

--- Platonic/settings.py
+++ Platonic/settings.py
@@ -271,11 +271,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # For production (example with SendGrid):
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zishanzubaer7@gmail.com'
-EMAIL_HOST_PASSWORD = 'qcns hmna owyr jcdw'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 
